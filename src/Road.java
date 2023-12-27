a0c072ed7a507ffc84312af4f668879ae81e7153
Mahdi Asghari <mahdiasghari@Mahdis-MacBook-Pro.local>
2023-12-27 13:50:27 +0100

    Adding a basic Road class which inherits from Tile

--- src/Road.java
+++ src/Road.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class Road {
+public class Road extends Tile{
+    Boolean hasBranch = false;
+    Boolean hasCactus = false;
+    public Road(Boolean hasBranch, Boolean hasCactus) {
+        super("Road");
+        this.hasBranch = hasBranch;
+        this.hasCactus = hasCactus;
+    }
+
+    @Override
+    public String toString() {
+        String out =  "Tile{" + "type=" + getType();
+        if (hasCactus) {out += "perk=has a cactus";}
+        if (hasBranch) {out += "perk=has a branch";}
+        out += "}";
+        return out;
+    }
 }
