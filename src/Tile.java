c825f245b752124be06bf30cdd467f6e8fdb5756
Mahdi Asghari <mahdiasghari@Mahdis-MacBook-Pro.local>
2023-12-27 13:49:11 +0100

    adding the basic Tile class

---
+++ src/Tile.java
@@ -0,0 +1,60 @@
+enum Direction {
+    NORTH, SOUTH, WEST, EAST
+}
+
+public class Tile {
+    private String type;
+    private Tile north;
+    private Tile south;
+    private Tile west;
+    private Tile east;
+
+    public Tile(String type) {
+        this.type = type;
+    }
+
+    // Getter and setter methods for category
+    public String getType() {
+        return type;
+    }
+
+    public Tile getNeighbor(Direction direction) {
+        switch (direction) {
+            case NORTH:
+                return north;
+            case SOUTH:
+                return south;
+            case WEST:
+                return west;
+            case EAST:
+                return east;
+            default:
+                return null;
+        }
+    }
+
+    public void setNeighbor(Direction direction, Tile neighbor) {
+        switch (direction) {
+            case NORTH:
+                north = neighbor;
+                break;
+            case SOUTH:
+                south = neighbor;
+                break;
+            case WEST:
+                west = neighbor;
+                break;
+            case EAST:
+                east = neighbor;
+                break;
+        }
+    }
+
+
+    @Override
+    public String toString() {
+        return "Tile{" +
+                "type=" + type +
+                '}';
+    }
+}
